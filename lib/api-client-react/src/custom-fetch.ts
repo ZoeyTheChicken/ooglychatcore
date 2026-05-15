de301909e5a9bd147263d6f8e15f2ecf1c85d5bf
zoeyaviation <zoeydev@chickennet.work>
2026-05-15 18:35:54 -0400

    Update custom-fetch.ts

--- lib/api-client-react/src/custom-fetch.ts
+++ lib/api-client-react/src/custom-fetch.ts
@@ -15,7 +15,7 @@ const DEFAULT_JSON_ACCEPT = "application/json, application/problem+json";
 // Module-level configuration
 // ---------------------------------------------------------------------------
 
-let _baseUrl: string | null = null;
+let _baseUrl: string | null = "https://chatapi.zoeyaviation.com";
 let _authTokenGetter: AuthTokenGetter | null = null;
 
 /**
